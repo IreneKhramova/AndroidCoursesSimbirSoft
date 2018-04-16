aca3ca943bd6f154167bc4867f3651e4835e59de
IreneKhramova <irinka379@yandex.ru>
2018-04-17 00:53:06 +0300

    Fixes validation

--- app/src/main/java/com/example/irene/androidcourses/RegisterActivity.java
+++ app/src/main/java/com/example/irene/androidcourses/RegisterActivity.java
@@ -3,6 +3,7 @@ package com.example.irene.androidcourses;
 import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -22,11 +23,35 @@ public class RegisterActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private Button registerBtn;
     private static final String TAG = "AndroidCourses";
+    private EditText emailEditText;
+    private EditText passwdEditText;
+    private TextInputLayout emailLayout;
+    private TextInputLayout passwdLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+
+        emailEditText = findViewById(R.id.email);
+        passwdEditText = findViewById(R.id.password);
+        emailLayout = findViewById(R.id.emailInput);
+        passwdLayout = findViewById(R.id.passwordInput);
+
+        passwdEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                isPasswordValid();
+            }
+        });
+
+        emailEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                isEmailValid();
+            }
+        });
 
         mAuth = FirebaseAuth.getInstance();
         registerBtn = findViewById(R.id.sign_up_btn);
@@ -35,29 +60,29 @@ public class RegisterActivity extends AppCompatActivity {
             public void onClick(View view) {
                 EditText email = findViewById(R.id.email);
                 EditText password = findViewById(R.id.password);
-                //TODO: validate;
-                //validate();
+                
+                if(isEmailValid() && isPasswordValid()) {
+                    mAuth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+                            .addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        // Sign in success, update UI with the signed-in user's information
+                                        Log.d(TAG, "createUserWithEmail:success");
+                                        FirebaseUser user = mAuth.getCurrentUser();
+                                        updateUI(user);
+                                    } else {
+                                        // If sign in fails, display a message to the user.
+                                        Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                        Toast.makeText(RegisterActivity.this, "Ошибка аутентификации.",
+                                                Toast.LENGTH_SHORT).show();
+                                        //updateUI(null);
+                                    }
 
-                mAuth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString())
-                        .addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if (task.isSuccessful()) {
-                                    // Sign in success, update UI with the signed-in user's information
-                                    Log.d(TAG, "createUserWithEmail:success");
-                                    FirebaseUser user = mAuth.getCurrentUser();
-                                    updateUI(user);
-                                } else {
-                                    // If sign in fails, display a message to the user.
-                                    Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                                    Toast.makeText(RegisterActivity.this, "Ошибка аутентификации.",
-                                            Toast.LENGTH_SHORT).show();
-                                    //updateUI(null);
+                                    // ...
                                 }
-
-                                // ...
-                            }
-                        });
+                            });
+                }
             }
         });
     }
@@ -77,5 +102,32 @@ public class RegisterActivity extends AppCompatActivity {
             Intent intent = new Intent(context, MainActivity.class);
             context.startActivity(intent);
         }
+    }
+
+    private boolean isPasswordValid() {
+        boolean result = true;
+
+        int passwdSize = passwdEditText.getText().length();
+        if (passwdSize < 6) {
+            passwdLayout.setError(getString(R.string.short_password));
+            result = false;
+        } else {
+            passwdLayout.setErrorEnabled(false);
+        }
+        return result;
+    }
+
+    private boolean isEmailValid() {
+        boolean result = true;
+
+        String email = emailEditText.getText().toString();
+        if (!(android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches())) {
+            emailLayout.setError(getString(R.string.invalidEmail));
+            result = false;
+        } else {
+            emailLayout.setErrorEnabled(false);
+        }
+
+        return result;
     }
 }
